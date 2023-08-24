bf47f63f6e137c8ceccb48306293cf947bffcf9f
Alina Vorontsova <vorontsova-alina@mail.ru>
2023-08-24 19:51:07 +0300

    feat: add new app initializing functions with new test constants

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from common import db, db_url, TEST_USERNAME, TEST_PASSWORD
 from main import app, socketio
+from common import (
+    db, db_url, 
+    TEST_USERNAME1, TEST_PASSWORD1, 
+    TEST_USERNAME2, TEST_PASSWORD2, 
+    TEST_GAME_ID, 
+    TEST_USER1_ID, TEST_USER2_ID, 
+    TEST_USER1_SHAPE, TEST_USER2_SHAPE, 
+    TEST_USER1_RESULT, TEST_USER2_RESULT
+)
 
 
 def init_users():
     from common.users_db import User
 
-    if User.find_by_username(TEST_USERNAME) is None:
-        User.create(TEST_USERNAME, TEST_PASSWORD)
+    if User.find_by_username(TEST_USERNAME1) is None:
+        User.create(TEST_USERNAME1, TEST_PASSWORD1)
+    
+    if User.find_by_username(TEST_USERNAME2) is None:
+        User.create(TEST_USERNAME2, TEST_PASSWORD2)
+
+
+def init_game():
+    from game.game_db import Game, GameActPerUser
+
+    if Game.find_by_id(TEST_GAME_ID) is None:
+        Game.create(id=TEST_GAME_ID)
+
+    GameActPerUser.create(game_id=TEST_GAME_ID, user_id=TEST_USER1_ID, shape=TEST_USER1_SHAPE, result=TEST_USER1_RESULT)
+    GameActPerUser.create(game_id=TEST_GAME_ID, user_id=TEST_USER2_ID, shape=TEST_USER2_SHAPE, result=TEST_USER2_RESULT)
 
 
 with app.app_context():
@@ -18,6 +39,7 @@ with app.app_context():
         db.create_all()
 
     init_users()
+    init_game()
     db.session.commit()
 
 app.after_request(db.with_autocommit)
